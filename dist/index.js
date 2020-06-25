6ce55435b371be7a9bef0aad5a784f2107d16162
hayazaki <toru.hayazaki@gmail.com>
2020-06-25 10:55:58 +0900

    how to use index signature

--- dist/index.js
+++ dist/index.js
@@ -7,4 +7,9 @@ let c = {
     banana: b,
 };
 let d = c.apple * 4;
+let h = {};
+h['test'] = 100;
+// h['test2'] = 'a'  // Type '"a"' is not assignable to type 'number'.
+console.log(h['test']);
+console.log(h['test2']);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,9 @@ let c = {
   banana: b,
 }
 let d = c.apple * 4
+
+let h: {[key: string]: number} = {}
+h['test'] = 100
+// h['test2'] = 'a'  // Type '"a"' is not assignable to type 'number'.
+console.log(h['test'])
+console.log(h['test2'])
